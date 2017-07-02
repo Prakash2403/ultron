64b5535c2b56aba391f6929539ea792a460c74a0
Monal5031 <201652013@iiitvadodara.ac.in>
2017-07-02 13:29:27 -0700

    added userid and expanduser

--- tests/search_file_test.py
+++ tests/search_file_test.py
@@ -1,5 +1,12 @@
+import os
+import pwd
+
 from ultron.actions.search_file import Search
 
 
 if __name__ == '__main__':
-    Search('test.txt', '/home/monal/Desktop/test').execute()
+    Search('test.txt',
+           os.path.expanduser('/home/' +
+                              pwd.getpwuid(os.getuid()).pw_name +
+                              '/Desktop/test')
+           ).execute()
