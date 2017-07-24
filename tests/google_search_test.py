23bcac7d310b7ab8ea1773133d3ef7948f6ed5df
neelansh <96shubh@gmail.com>
2017-07-24 16:47:24 +0530

    Add Google Search for a search query

--- tests/google_search_test.py
+++ tests/google_search_test.py
@@ -1,4 +1,16 @@
-from ultron.actions.google_search import googleSearch
+from ultron.actions.google_search import GoogleSearch
 
 if __name__== '__main__':
-    googleSearch().execute(search_query='Kamaal Rashid Khan')
+    googleSearch = GoogleSearch('Kamaal Rashid Khan')
+
+    googleSearch.pre_execute()
+    googleSearch.execute()
+    googleSearch.post_execute()
+
+    print('\nNames -->')
+    for each_name in googleSearch.get_names():
+        print('\t'+each_name)
+
+    print('\nLinks -->')
+    for each_link in googleSearch.get_links():
+        print('\t'+each_link)
--- ultron/actions/google_search.py
+++ ultron/actions/google_search.py
@@ -1,19 +1,39 @@
 from ultron.actions import Action
-import webbrowser
+import requests
+from bs4 import BeautifulSoup
 
-class googleSearch(Action):
-    def __init__(self):
-        self.search_engine = 'http://www.google.com/?#q='
-        self.search_query = ''
-
-    def execute(self, search_query, **kwargs):
+class GoogleSearch(Action):
+    def __init__(self, search_query):
         self.search_query = search_query
-        webbrowser.open_new_tab(self.search_engine + search_query)
-        pass
+        self.links = []
+        self.names = []
 
     def pre_execute(self):
         pass
 
-    def post_execute(self):
-        #Close the newly opened Tab
+    def execute(self, *args, **kwargs):
+
+        url = 'http://www.google.co.in/search?q='+self.search_query.replace(' ', '+')
+
+        res = requests.get(url)
+        web_data = BeautifulSoup(res.text, "lxml")
+
+        tags = web_data.find_all('h3')
+
+        for each_tag in tags:
+
+            link = each_tag.find_all('a')
+
+            if(len(link)>0):
+                self.names.append(each_tag.text)
+                self.links.append(link[0].get('href').replace('/url?q=', ''))
         pass
+
+    def post_execute(self):
+        pass
+
+    def get_links(self):
+        return self.links
+
+    def get_names(self):
+        return self.names
