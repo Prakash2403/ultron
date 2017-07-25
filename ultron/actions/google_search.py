a5949c22fb70436eb745bee2227307642c43493d
Neelansh Sahai <96shubh@gmail.com>
2017-07-25 15:45:10 +0530

    Update google_search.py

--- ultron/actions/google_search.py
+++ ultron/actions/google_search.py
@@ -2,6 +2,7 @@ from ultron.actions import Action
 import requests
 from bs4 import BeautifulSoup
 
+
 class GoogleSearch(Action):
     def __init__(self, search_query):
         self.search_query = search_query
@@ -13,7 +14,8 @@ class GoogleSearch(Action):
 
     def execute(self, *args, **kwargs):
 
-        url = 'http://www.google.co.in/search?q='+self.search_query.replace(' ', '+')
+        url = 'http://www.google.co.in/search?q='+
+        self.search_query.replace(' ', '+')
 
         res = requests.get(url)
         web_data = BeautifulSoup(res.text, "lxml")
@@ -24,7 +26,7 @@ class GoogleSearch(Action):
 
             link = each_tag.find_all('a')
 
-            if(len(link)>0):
+            if(len(link) > 0):
                 self.names.append(each_tag.text)
                 self.links.append(link[0].get('href').replace('/url?q=', ''))
         pass
@@ -37,4 +39,3 @@ class GoogleSearch(Action):
 
     def get_names(self):
         return self.names
-    
